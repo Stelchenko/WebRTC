097bd07e482a48c0f8498b126e053a62cd62361b
stelchenko <stelchenkoav@gmail.com>
2021-12-23 16:36:58 +0300

    fix

--- src/screens/webrtcScreen/index.tsx
+++ src/screens/webrtcScreen/index.tsx
@@ -1,8 +1,21 @@
-import React, {useEffect, useRef, useState} from "react";
-import {Button, Platform, Text, TextInput, TouchableHighlight, TouchableOpacity, View} from "react-native";
-import {TwilioVideo, TwilioVideoLocalView, TwilioVideoParticipantView} from "react-native-twilio-video-webrtc";
-import {styles} from "./style";
-import {checkMultiple, PERMISSIONS} from "react-native-permissions";
+import React, {
+  useEffect, useRef, useState
+} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  TouchableHighlight, Platform
+} from 'react-native';
+import {
+  TwilioVideoLocalView, // to get local view
+  TwilioVideoParticipantView, //to get participant view
+  TwilioVideo
+} from 'react-native-twilio-video-webrtc';
+import {PERMISSIONS, requestMultiple} from "react-native-permissions";
+
 
 const WebrtcScreen = () => {
   const [isAudioEnabled, setIsAudioEnabled] = useState(true);
@@ -16,14 +29,15 @@ const WebrtcScreen = () => {
   const twilioRef = useRef(null);
 
   const GetAllPermissions = () => {
+    console.log(Platform.OS)
     if (Platform.OS === "ios") {
-      checkMultiple([PERMISSIONS.IOS.CAMERA, PERMISSIONS.IOS.MICROPHONE]).then((statuses) => {
+      console.log('asdasdasd')
+      requestMultiple([PERMISSIONS.IOS.CAMERA, PERMISSIONS.IOS.MICROPHONE]).then((statuses) => {
         console.log('Camera', statuses[PERMISSIONS.IOS.CAMERA]);
         console.log('Micro', statuses[PERMISSIONS.IOS.MICROPHONE]);
       })
-    }
-    else if (Platform.OS === "android") {
-      checkMultiple([PERMISSIONS.ANDROID.CAMERA, PERMISSIONS.ANDROID.RECORD_AUDIO]).then((statuses) => {
+    } else if (Platform.OS === "android") {
+      requestMultiple([PERMISSIONS.ANDROID.CAMERA, PERMISSIONS.ANDROID.RECORD_AUDIO]).then((statuses) => {
         console.log('Camera', statuses[PERMISSIONS.ANDROID.CAMERA]);
         console.log('Micro', statuses[PERMISSIONS.ANDROID.RECORD_AUDIO]);
       })
@@ -31,82 +45,69 @@ const WebrtcScreen = () => {
   }
 
   useEffect(() => {
-    console.log('ASDASDASD')
     GetAllPermissions();
   })
 
-
-
   const _onConnectButtonPress = () => {
-    console.log('connect button press')
-    twilioRef.current.connect({ roomName: roomName, accessToken: token});
-    setStatus('connecting');
-    console.log(status)
+    console.log("in on connect button preess");
+    twilioRef.current.connect({roomName: roomName, accessToken: token})
+    setStatus('connecting')
+    console.log(status);
   }
-
   const _onEndButtonPress = () => {
-    twilioRef.current.disconnect();
-  };
-
+    twilioRef.current.disconnect()
+  }
   const _onMuteButtonPress = () => {
-    twilioRef.current
-      .setLocalAudioEnabled(!isAudioEnabled)
-      .then(isEnabled => setIsAudioEnabled(isEnabled));
-  };
-
+    // on cliking the mic button we are setting it to mute or viceversa
+    twilioRef.current.setLocalAudioEnabled(!isAudioEnabled)
+      .then(isEnabled => setIsAudioEnabled(isEnabled))
+  }
   const _onFlipButtonPress = () => {
-    twilioRef.current.flipCamera();
-  };
+    // switches between fronst camera and Rare camera
+    twilioRef.current.flipCamera()
+  }
+  const _onRoomDidConnect = () => {
+    console.log("room did connected");
+    setStatus('connected')
+    // console.log("over");
+  }
+  const _onRoomDidDisconnect = ({roomName, error}) => {
+    console.log("ERROR: ", JSON.stringify(error))
+    console.log("disconnected")
 
-  const _onRoomDidConnect = ({roomName, error}) => {
-    console.log('onRoomDidConnect: ', roomName);
-
-    setStatus('connected');
-  };
-
-  const _onRoomDidDisconnect = ({ roomName, error }) => {
-    console.log('[Disconnect]ERROR: ', error);
-
-    setStatus('disconnected');
-  };
-
-  const _onRoomDidFailToConnect = error => {
-    console.log('[FailToConnect]ERROR: ', error);
-    console.log('failed to connect')
-    setStatus('disconnected');
-  };
-
-  const _onParticipantAddedVideoTrack = ({ participant, track }) => {
-    console.log('onParticipantAddedVideoTrack: ', participant, track);
-
-    setVideoTracks(
-      new Map([
+    setStatus('disconnected')
+  }
+  const _onRoomDidFailToConnect = (error) => {
+    console.log("ERROR: ", JSON.stringify(error));
+    console.log("failed to connect");
+    setStatus('disconnected')
+  }
+  const _onParticipantAddedVideoTrack = ({participant, track}) => {
+    // call everytime a participant joins the same room
+    console.log("onParticipantAddedVideoTrack: ", participant, track)
+    setVideoTracks(new Map([
         ...videoTracks,
-        [
-          track.trackSid,
-          { participantSid: participant.sid, videoTrackSid: track.trackSid },
-        ],
+        [track.trackSid, {participantSid: participant.sid, videoTrackSid: track.trackSid}]
       ]),
     );
-  };
 
-  const _onParticipantRemovedVideoTrack = ({ participant, track }) => {
-    console.log('onParticipantRemovedVideoTrack: ', participant, track);
+    console.log("this.state.videoTracks", videoTracks);
+  }
+  const _onParticipantRemovedVideoTrack = ({participant, track}) => {
+    // gets called when a participant disconnects.
+    console.log("onParticipantRemovedVideoTrack: ", participant, track)
+    const videoTracksLocal = videoTracks
+    videoTracksLocal.delete(track.trackSid)
+    setVideoTracks({...videoTracksLocal})
+  }
 
-    const videoTracksLocal = videoTracks;
-    videoTracksLocal.delete(track.trackSid);
-
-    setVideoTracks(videoTracksLocal);
-  };
 
   return (
-    <View style={styles.container} >
+
+    <View style={styles.container}>
       {
         status === 'disconnected' &&
         <View>
-          <Text style={styles.welcome}>
-            React Native Twilio Video
-          </Text>
           <View style={styles.spacing}>
             <Text style={styles.inputLabel}>Room Name</Text>
             <TextInput style={styles.inputBox}
@@ -123,7 +124,8 @@ const WebrtcScreen = () => {
                        onChangeText={(text) => setToken(text)}
             />
           </View>
-          <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={_onConnectButtonPress}>
+          <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]}
+                              onPress={_onConnectButtonPress}>
             <Text style={styles.Buttontext}>Connect</Text>
           </TouchableHighlight>
         </View>
@@ -134,7 +136,9 @@ const WebrtcScreen = () => {
           {
             status === 'connected' &&
             <View style={styles.remoteGrid}>
-              <TouchableOpacity style = {styles.remoteVideo} onPress={()=>{setIsButtonDisplay(!isButtonDisplay)}} >
+              <TouchableOpacity style={styles.remoteVideo} onPress={() => {
+                setIsButtonDisplay(!isButtonDisplay)
+              }}>
                 {
                   Array.from(videoTracks, ([trackSid, trackIdentifier]) => {
                     return (
@@ -149,12 +153,12 @@ const WebrtcScreen = () => {
               </TouchableOpacity>
               <TwilioVideoLocalView
                 enabled={true}
-                style = {isButtonDisplay ? styles.localVideoOnButtonEnabled : styles.localVideoOnButtonDisabled}
+                style={isButtonDisplay ? styles.localVideoOnButtonEnabled : styles.localVideoOnButtonDisabled}
               />
             </View>
           }
           <View
-            style = {
+            style={
               {
                 display: isButtonDisplay ? "flex" : "none",
                 position: "absolute",
@@ -169,7 +173,7 @@ const WebrtcScreen = () => {
                 // zIndex: 2,
                 zIndex: isButtonDisplay ? 2 : 0,
               }
-            } >
+            }>
             <TouchableOpacity
               style={
                 {
@@ -228,15 +232,123 @@ const WebrtcScreen = () => {
         </View>
       }
       <TwilioVideo
-        ref={ twilioRef }
-        onRoomDidConnect={ _onRoomDidConnect }
-        onRoomDidDisconnect={ _onRoomDidDisconnect }
-        onRoomDidFailToConnect= { _onRoomDidFailToConnect }
-        onParticipantAddedVideoTrack={ _onParticipantAddedVideoTrack }
-        onParticipantRemovedVideoTrack= { _onParticipantRemovedVideoTrack }
+        ref={twilioRef}
+        onRoomDidConnect={_onRoomDidConnect}
+        onRoomDidDisconnect={_onRoomDidDisconnect}
+        onRoomDidFailToConnect={_onRoomDidFailToConnect}
+        onParticipantAddedVideoTrack={_onParticipantAddedVideoTrack}
+        onParticipantRemovedVideoTrack={_onParticipantRemovedVideoTrack}
       />
     </View>
-  );
+  )
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  callContainer: {
+    flex: 1,
+    position: "absolute",
+    bottom: 0,
+    top: 0,
+    left: 0,
+    right: 0,
+    minHeight: "100%"
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center',
+    paddingTop: 40
+  },
+  input: {
+    height: 50,
+    borderWidth: 1,
+    marginRight: 70,
+    marginLeft: 70,
+    marginTop: 50,
+    textAlign: 'center',
+    backgroundColor: 'white'
+  },
+  button: {
+    marginTop: 100
+  },
+  localVideoOnButtonEnabled: {
+    bottom: ("40%"),
+    width: "35%",
+    left: "64%",
+    height: "25%",
+    zIndex: 2,
+  },
+  localVideoOnButtonDisabled: {
+    bottom: ("30%"),
+    width: "35%",
+    left: "64%",
+    height: "25%",
+    zIndex: 2,
+  },
+  remoteGrid: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  remoteVideo: {
+    width: 300,
+    height: 500,
+    zIndex: 1,
+  },
+  optionsContainer: {
+    position: "absolute",
+    left: 0,
+    bottom: 0,
+    right: 0,
+    height: 100,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    zIndex: 2,
+  },
+  optionButton: {
+    width: 60,
+    height: 60,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 100 / 2,
+    backgroundColor: 'grey',
+    justifyContent: 'center',
+    alignItems: "center"
+  },
+  spacing: {
+    padding: 10
+  },
+  inputLabel: {
+    fontSize: 18
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderRadius: 30,
+  },
+  loginButton: {
+    backgroundColor: "#1E3378",
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 20,
+    marginTop: 10
+  },
+  Buttontext: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 18
+  },
+  inputBox: {
+    borderBottomColor: '#cccccc',
+    fontSize: 16,
+    borderBottomWidth: 1
+  },
+});
 
 export default WebrtcScreen
